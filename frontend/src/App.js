517658336b31324619cd62583020e6da3f9fd512
amritanandsingh <amritanandsingh999@gmail.com>
2023-10-25 22:54:18 +0530

    Footer.jsx and Header.jsx is Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
-
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
 function App() {
   return (
-    <h1>
-      Welcome To ProShop
-    </h1>
+    <>
+      <Header/>
+      <main className="py-3">
+        <Container>
+        <h1> Welcome To ProShop </h1>
+        </Container>
+      </main>
+      
+    </>
   );
 }
 
