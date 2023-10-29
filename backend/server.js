b73d36db229c1a7df9a493167093252818e055df
amritanandsingh <amritanandsingh999@gmail.com>
2023-10-29 22:23:12 +0530

    mongoDB is ready

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from './config/db.js';
 import products from './data/products.js';
 const port = process.env.PORT || 5000 ;
+connectDB(); // connect to MongoDB
+
 const app = express();
 
 app.get('/' , (req,res)=>{
@@ -21,5 +24,4 @@ app.get('/api/products/:id',(req,res)=>{
 
 app.listen(port , ()=>{
     console.log(`server is running on port ${port} `);
-
 });
